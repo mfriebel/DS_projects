809502ef6e068cd9d594254b65ec77a1cbdfda7a
mfriebel <matfriebel@gmail.com>
2020-12-17 20:15:56 +0100

    took out testing data, added AutoReg, added ARIMA

--- week05/time_series.py
+++ week05/time_series.py
@@ -10,9 +10,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import TimeSeriesSplit, cross_val_score
 
-from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from statsmodels.tsa.arima.model import ARIMA
+import pickle
+
+import warnings
+warnings.filterwarnings("ignore") # specify to ignore warning messages
 
 #Read Data
 df = pd.read_csv("data/TG_STAID002759.txt", skiprows=19, index_col=1, parse_dates=True)
@@ -26,7 +31,9 @@ df_clean['timestep'] = list(range(len(df_clean.index)))
 # Train-test-split
 train = df_clean.iloc[:-365]
 test = df_clean.iloc[-365:]
+test.to_csv('test.csv')
 
+'''Remove Seasonality'''
 # Seasonality
 train = season_feature(train)
 
@@ -39,13 +46,25 @@ m_season.fit(X_season,y_season)
 
 train['seasonal_trend'] = m_season.predict(X_season)
 
+
+'''Add Lags from remainder'''
 # Calculate Remainder
-train['remainder'] = train['temp'] - train['seasonal_trend']
+remainder = train['temp'] - train['seasonal_trend']
+train['remainder'] = remainder
+
+'''Plot remainder'''
+
+plot_pacf(train['remainder'])
+plt.show()
+plot_acf(train['remainder'])
+plt.show()
 
 # Add lag features
-train = create_lag_features(train, 2)
+train = create_lag_features(train, 3)
 
-# Full Model
+
+'''Manual AR'''
+
 X_full = train.drop(['temp', 'seasonal_trend', 'remainder'], axis=1)
 y_full = train['temp']
 
@@ -53,29 +72,60 @@ m_full = LinearRegression()
 m_full.fit(X_full, y_full)
 
 train['full_model'] = m_full.predict(X_full)
-print(f'Training-Score: {round(m_full.score(X_full, y_full),3)}')
+print(f'Training-Score (Manual AR): {round(m_full.score(X_full, y_full),3)}')
 
-sns.lineplot(x=train.loc['2000'].index, y='temp', data=train.loc['2000'])
-sns.lineplot(x=train.loc['2000'].index, y='full_model', data=train.loc['2000'])
-plt.show()
 
-# Cross-Validation
+'''Cross-Validation'''
+
 time_series_split = TimeSeriesSplit(n_splits=5)
 splits = time_series_split.split(X_full, y_full) 
-cv = cross_val_score(estimator=m_full, X=X_full, y=y_full, cv=splits)
-cv.mean()
-print(f'CV-Score: {round(cv.mean(),3)}')
+cv_manual_ar = cross_val_score(estimator=m_full, X=X_full, y=y_full, cv=splits)
+print(f'CV-Score (Manual AR): {round(cv_manual_ar.mean(),3)}')
 
-# Test Model
-test = season_feature(test)
-test['seasonal_trend'] = m_season.predict(test.drop(['temp'], axis=1))
-test['remainder'] = test['temp'] - test['seasonal_trend']
-test = create_lag_features(test, 2)
-X_test = test.drop(['temp', 'seasonal_trend', 'remainder'], axis=1)
-test['full_model'] = m_full.predict(X_test)
-y_test = test['temp']
-print(f'Test-Score: {round(m_full.score(X_test, y_test),3)}')
 
-sns.lineplot(x=test.index, y='temp', data=test)
-sns.lineplot(x=test.index, y='full_model', data=test)
+'''AutoRegressive Model - Statsmodels (on data taking into account trend and seasonality)'''
+
+ar_model = AutoReg(y_season, lags=3, exog=X_season).fit()
+#print(ar_model.summary())
+prediction_ar = ar_model.predict()
+
+'''ARIMA Model - Statsmodels (on data taking into account trend and seasonality) - very slow!!''' 
+
+#arima_model = ARIMA(y_season, order=(1,0,1), exog=X_season).fit()
+#print(arima_model.summary())
+#prediction_arima = arima_model.predict()
+
+'''ARIMA Model - only on remainder '''
+
+arima_model = ARIMA(remainder, order=(2,0,2), freq='D').fit()
+prediction_arima = arima_model.predict()
+prediction_arima.name = 'Arima_lags'
+
+# Use prediction of ARIMA Model as feature(includes lags2 , MA 2) for LinearRegression 
+X_arima = X_season.join(prediction_arima)
+m_arima = LinearRegression()
+m_arima.fit(X_arima, y_season)
+outcome_arima = pd.Series(m_arima.predict(X_arima), index=y_season.index)
+
+
+'''Plot data as residuals '''
+#sns.lineplot(x=train.loc['2000'].index, y='temp', data=train.loc['2000'], label = 'true values')
+#sns.lineplot(x=train.loc['2000'].index, y='full_model', data=train.loc['2000'], label = 'Manual AR')
+sns.lineplot(x=train.loc['2000'].index, y=(train['temp'].loc['2000'] - train['full_model'].loc['2000']), label = 'Residuals Manual AR')
 plt.show()
+
+#sns.lineplot(x=train.loc['2000'].index, y='temp', data=train.loc['2000'], label = 'true values')
+#sns.lineplot(x=train.loc['2000'].index, y=prediction_ar.loc['2000'], label ='AR')
+sns.lineplot(x=train.loc['2000'].index, y=(train['temp'].loc['2000'] - prediction_ar.loc['2000'].loc['2000']), label = 'Residuals AutoReg')
+plt.show()
+
+#sns.lineplot(x=train.loc['2000'].index, y='temp', data=train.loc['2000'], label = 'true values')
+#sns.lineplot(x=train.loc['2000'].index, y=outcome_arima.loc['2000'], label ='ARIMA')
+sns.lineplot(x=train.loc['2000'].index, y=(train['temp'].loc['2000'] - outcome_arima.loc['2000']), label = 'Residuals ARIMA')
+plt.show()
+
+
+
+'''Save Manual AR Model for testing'''
+pickle.dump(m_season, open('seasonal_model.pickle', 'wb'))
+pickle.dump(m_full, open('full_model.pickle', 'wb'))
