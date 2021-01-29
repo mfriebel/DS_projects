7f38964d85af1ed90c9f671338b9f16b7d312e48
mfriebel <matfriebel@gmail.com>
2021-01-29 14:34:20 +0100

    best scoring approach, added test evaluation

--- week09/MNIST_CNN.py
+++ week09/MNIST_CNN.py
@@ -17,22 +17,20 @@ y_train = to_categorical(y_train, 10)
 #%%%
 m = Sequential([
     # 1st CV layer
-    Conv2D(40, kernel_size=(3, 3), strides=(2, 2),
+    Conv2D(32, kernel_size=(3, 3), strides=(2, 2),
            padding="same", activation="relu",
            input_shape=(28, 28, 1)),
-    #Conv2D(40, kernel_size=(3, 3), strides=(2, 2),
-    #       padding="same", activation="relu"),
-    MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same"),
-    BatchNormalization(),
-    Conv2D(40, kernel_size=(3, 3), strides=(2, 2),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same"),
+    #BatchNormalization(),
+    Conv2D(32, kernel_size=(3, 3), strides=(2, 2),
            padding="same", activation="relu",
            input_shape=(28, 28, 1)),
-    Conv2D(40, kernel_size=(3, 3), strides=(2, 2),
+    Conv2D(32, kernel_size=(3, 3), strides=(2, 2),
            padding="same", activation="relu"),
-    MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same"),
+    MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same"),
     BatchNormalization(),
     Flatten(),
-    Dropout(0.5),
+    Dropout(0.3),
     Dense(units=50, activation = 'relu'),
     BatchNormalization(),
     Dense(units=10, activation = 'softmax'),
@@ -40,6 +38,12 @@ m = Sequential([
 ])
 callback = EarlyStopping(monitor='val_loss', patience=3)
 m.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+#%%
+m.summary()
+#%%
+for layer in m.layers:
+       print(layer)
 
 #%%
 # Train the model
@@ -63,8 +67,29 @@ plt.xlabel('epochs')
 plt.legend()
 plt.show()
 
-#%%
-K.clear_session()
-# %%
 
+#%%
+X_test = X_test.reshape(10000, 28, 28, 1)
+start = 50
+y_pred = m.predict(X_test[start:start+10])
+y_pred_classes = m.predict_classes(X_test[start:start+10])
+y_true = y_test[start:start+10]
+
+y_pred_prob = [prediction.max() for prediction in y_pred]
+
+plt.figure(figsize=(10,10))
+for i in range(10):
+       
+       plt.subplot(3, 4, i+1)
+       plt.imshow(X_test[start+i].reshape(28, 28))
+       plt.title((str(y_pred_classes[i]) + ":    " + str(round(y_pred_prob[i], 5))))
+
+#%%
+from sklearn.metrics import accuracy_score
+
+y_pred_all = m.predict_classes(X_test)
+
+print(f'The accuracy score(test) of the model is: {accuracy_score(y_test, y_pred_all)}')
+# %%
+K.clear_session()
 # %%
